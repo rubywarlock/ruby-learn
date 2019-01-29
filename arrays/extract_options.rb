763b124680c686186fd84c798745cf1071097906
Vitaly Simakov <faetonvs@gmail.com>
2019-01-29 15:52:07 +0500

    hash extract next

--- arrays/extract_options.rb
+++ arrays/extract_options.rb
@@ -3,17 +3,22 @@ require "active_support/core_ext/object"
 # .extract_options! is a Rails method
 # .present? is a Rails method
 
-def extract_options(target, *res)
+def extract_opt(target, *res)
   @scope = { as: "like scope" }
-  options = { as: "like options" }
+  # options = { as: "like options" }
   options = res.extract_options!
 
   options[:as] ||= @scope[:as] if @scope[:as].present?
 
+  res.map!(&:to_sym)
+
   yield(eval(target.to_s))
 end
 
-extract_options(:options, :hello => :world) do |target|
-  print target
+def create_opts(*res)
+  extract_opt(:options, *res) do |target|
+    print target
+  end
 end
 
+create_opts :h, :w
