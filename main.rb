9cd4136894feb1513e7d59acf50537647cad69cb
Vitaly Simakov <faetonvs@gmail.com>
2019-01-30 13:32:40 +0500

    add require yield

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
+# require './yield'
 # require './ext_method'
 # require './hash_one'
 # require './block'
 # require './predicate'
 # require './modules'
-require './super'
+# require './super'
 
 # require './arrays/extract_options'
 
