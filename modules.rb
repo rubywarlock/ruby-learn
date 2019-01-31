f0bd9b4277248c477b66ac7cbdb0b8d18d53a3cc
Vitaly Simakov <faetonvs@gmail.com>
2019-01-31 17:09:53 +0500

    modules extends

--- modules.rb
+++ modules.rb
@@ -1,3 +1,4 @@
+=begin
 module Parent
   def parent_puts
     puts "parent puts"
@@ -25,3 +26,36 @@ IClass.new.child_puts
 IClass.new.parent_puts
 
 IClass::Other.parent_puts
+=end
+
+module SomeModule
+
+end
+
+module OtherModule
+  def other_puts
+    puts "other"
+  end
+end
+
+module ThirdModule
+  def third_puts
+    puts "Third"
+  end
+
+  def other_puts
+    puts "other"
+  end
+end
+
+module BuilderModule
+  module SomeModule
+    include ThirdModule
+  end
+end
+
+class IClass
+  extend BuilderModule::SomeModule
+end
+
+IClass.other_puts
