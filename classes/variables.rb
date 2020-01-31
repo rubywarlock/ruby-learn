995897cedd113885b6405421088dc123438c2416
Vitaly Simakov <faetonvs@gmail.com>
2020-01-31 16:02:53 +0500

    class variables

--- classes/variables.rb
+++ classes/variables.rb
@@ -1,33 +1,25 @@
+def log(value = nil); puts; puts value; end
+
 class Test
-  @@class_var = "null"
-  I_CONST = "i constant"
+  attr_accessor :var
+
+  @variable_two = "self variable_two"
+  @@class_var = "default value"
+  I_CONST = "i::big variable"
 
   def initialize
     @var = "cool"
-    @@class_var = "i class var"
-  end
-
-  def var=(value)
-    @var = value
-  end
-
-  def var
-    @var
-  end
-
-  def class_var=(value)
-    @@class_var = value
-  end
-
-  def class_var
-    @@class_var
-  end
-
-  def i_const
-    I_CONST
   end
 
   class << self
+    def class_var
+      @@class_var
+    end
+
+    def class_var=(v)
+      @@class_var = v
+    end
+
     def self_var=(value)
       @self_var = value
     end
@@ -38,9 +30,12 @@ class Test
   end
 end
 
+Test::I_CONST = "fuck"
+log Test::I_CONST
+
 e = Test.new
-puts e.self_var
+#puts e
 
 Test.self_var = "text"
 
-puts Test.self_var
+#Test.class_var ="cool"
