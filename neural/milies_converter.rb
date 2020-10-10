48f5cd736c9ad04ed078a2eba41f0be3de3f4a5b
Vitaly Simakov <faetonvs@gmail.com>
2020-10-10 22:49:00 +0500

    add: original neuron - wrong and my neural - work

--- neural/milies_converter.rb
+++ neural/milies_converter.rb
@@ -1,7 +1,7 @@
-class Neuron
+class NeuronOriginal
   @@weight = 0.5
   @@last_error = 0
-  STEP = 0.00000001
+  SMOOTHING = 0.000001
 
   def processInputData(input)
     input * @@weight
@@ -13,9 +13,33 @@ class Neuron
 
   def train input, expected_result
     begin
-      @@last_error = processInputData(input)
+      actual_result = processInputData(input)
+      @@last_error = expected_result - actual_result
+      correction = @@last_error / actual_result
+      @@weight += correction * SMOOTHING
+      puts "actual: #{actual_result}; SMOOTHING: #{SMOOTHING}; @@last_error: #{@@last_error}; correction: #{correction}"
+    end while @@last_error > SMOOTHING || @@last_error < -SMOOTHING
+  end
+end
+
+class Neuron
+  @@weight = 0.5
+  @@last_error = 0
+  SMOOTHING = 0.00000001
+
+  def km_to_milles(input)
+    input * @@weight
+  end
+
+  def milles_to_km(output)
+    output / @@weight
+  end
+
+  def train input, expected_result
+    begin
+      @@last_error = km_to_milles(input)
       if @@last_error != expected_result
-        @@weight = @@weight + @@last_error * STEP
+        @@weight += @@last_error * SMOOTHING
       end
     end while @@last_error <= expected_result
   end
@@ -24,7 +48,8 @@ end
 neuron = Neuron.new
 neuron.train(100, 62.1371)
 
-puts neuron.processInputData(50)
+puts neuron.km_to_milles(50) # 31,0686
+puts neuron.milles_to_km(1) # 31,0686
 
 
 
