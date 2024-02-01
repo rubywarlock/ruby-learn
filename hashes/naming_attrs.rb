1a128362b295909c1794fd17fb293d4d7bc3b18d
Vitaly Simakov <v.simakov@tdm-tech.ru>
2024-02-01 21:56:59 +0500

    add: namming attr ruby 3+

--- hashes/naming_attrs.rb
+++ hashes/naming_attrs.rb
@@ -3,9 +3,25 @@
 def meth(options, attr: true)
   puts
   puts "==============================================="
+  puts "meth =========================================="
   pp options
   puts "==============================================="
-  puts
 end
 
 meth({ one: "ONE", two: "TWO" })
+
+def meth_2(**hash)
+  pp hash
+end
+
+begin
+  meth_2({ one: "ONE", two: "TWO" })
+rescue => _
+  puts
+  puts "==============================================="
+  puts "meth_2 ========================================"
+  puts 'error calling neth_2 with `meth_2({ one: "ONE", two: "TWO" })`'
+  puts 'right variant: meth_2(**{ one: "ONE", two: "TWO" })'
+  puts "==============================================="
+end
+
